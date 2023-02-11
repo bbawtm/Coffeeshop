333d65b623ca1922bf88c8eea2cb3286f20b4f2e
VadimPopov <vessims@gmail.com>
2023-02-11 12:14:18 +0300

    Map for St. Petersburg + simple bottom sheet

---
+++ Coffeeshop/ViewModel/BottomSheetViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BottomSheetViewController.swift
+//  Coffeeshop
+//
+//  Created by Vadim Popov on 08.02.2023.
+//
+
+import UIKit
+
+
+class BottomSheetViewController: UIViewController, UIAdaptivePresentationControllerDelegate {
+    
+    @IBOutlet var placeAddress: UILabel!
+    
+    public var providedAddress: String?
+    public var dismissClosure: (() -> Void)?
+    
+    override func viewDidLoad() {
+        placeAddress.text = providedAddress
+        self.presentationController?.delegate = self
+    }
+    
+    public func setPlaceAddress(_ addr: String) {
+        providedAddress = addr
+    }
+    
+    func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
+        self.dismissClosure?()
+    }
+    
+}
--- Coffeeshop/ViewModel/ViewController.swift
+++ Coffeeshop/ViewModel/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
     
     private let initRegionRadius = 20000.0
     
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
         
         
         mapView.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 59.938955, longitude: 30.315644), latitudinalMeters: initRegionRadius, longitudinalMeters: initRegionRadius), animated: true)
+        mapView.delegate = self
         
         for place in placesInfo.contents {
             guard let coord = place.coordinates else { continue }
@@ -32,7 +33,25 @@ class ViewController: UIViewController {
             mapView.addAnnotation(annotations)
         }
     }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        guard let annotation = view.annotation else { return }
+        
+        let sheetStoryboard = UIStoryboard.init(name: "BottomSheetSubscreen", bundle: Bundle.main)
+        let sheetViewController = sheetStoryboard.instantiateInitialViewController() as! BottomSheetViewController
+        sheetViewController.setPlaceAddress(annotation.title!!)
+        sheetViewController.dismissClosure = {
+            self.mapView.deselectAnnotation(view.annotation, animated: true)
+        }
 
-
+        if let sheet = sheetViewController.sheetPresentationController {
+            sheet.detents = [.medium(), .large()]
+            sheet.prefersGrabberVisible = true
+        }
+        self.present(sheetViewController, animated: true)
+    }
+    
+    
+    
 }
 
